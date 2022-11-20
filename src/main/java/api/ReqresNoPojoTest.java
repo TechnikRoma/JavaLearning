3c4592f212660ab3e55cb8acdcef5b762a24bf8d
Roman Rogozhnikov <akioyo@ya.ru>
2022-11-21 00:20:00 +0300

    No Pojo test reqres

---
+++ src/main/java/api/ReqresNoPojoTest.java
@@ -0,0 +1,67 @@
+package api;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class ReqresNoPojoTest {
+    private final static String URL = "https://reqres.in/";
+
+    @Test
+    public void checkAvatarsNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpec200());
+        Response response = given()
+                .when()
+                .get("api/users?page=2")
+                .then().log().all()
+                .body("page", equalTo(2))
+                .body("data.id", notNullValue())
+                .body("data.email", notNullValue())
+                .body("data.first_name", notNullValue())
+                .body("data.last_name", notNullValue())
+                .body("data.avatar", notNullValue())
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        List<String> emails = jsonPath.get("data.email");
+        List<Integer> ids = jsonPath.get("data.id");
+        List<String> avatars = jsonPath.get("data.avatar");
+        for (int i = 0; i < avatars.size(); i++) {
+            Assert.assertTrue(avatars.get(i).contains(ids.get(i).toString()));
+        }
+
+        Assert.assertTrue(emails.stream().allMatch(x -> x.endsWith("@reqres.in")));
+    }
+
+    @Test
+    public void successRegNoPojoTest() {
+        Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpec200());
+        Map<String, String> user = new HashMap<>();
+        user.put("email", "eve.holt@reqres.in");
+        user.put("password", "pistol");
+        Response response = given()
+                .body(user)
+                .when()
+                .post("api/register")
+                .then()
+                .log().all()
+              //  .body("id", equalTo(4))
+              //  .body("token", equalTo("QpwL5tke4Pnpja7X4"))
+                .extract().response();
+        JsonPath jsonPath = response.jsonPath();
+        int id = jsonPath.get("id");
+        String token = jsonPath.get("token");
+        Assert.assertEquals(4, id);
+        Assert.assertEquals("QpwL5tke4Pnpja7X4",token);
+    }
+}
